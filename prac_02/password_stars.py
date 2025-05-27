3c7afa9a60e8049f9fea2af72f46373c5881cf0b
Wang Runze <1678626701a@gmail.com>
2025-05-27 13:06:28 +0800

    Add 2 files for prac 2; Refactor password_stars into functions; Deleted todo comment

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,22 @@
-minimum_length = 10
-password = input(f"Enter your password(no shorter than {minimum_length} characters): ")
-while len(password) < minimum_length:
-    print(f"Password length cannot shorter than {minimum_length} characters")
+
+def main():
+    password = get_password()
+    print_asterisks(password)
+
+
+def print_asterisks(password):
+    """Print a line of asterisks with the length of password"""
+    print(len(password) * "*")
+
+
+def get_password():
+    """Get a valid password with a minimum length"""
+    minimum_length = 10
     password = input(f"Enter your password(no shorter than {minimum_length} characters): ")
-print(len(password) * "*")
+    while len(password) < minimum_length:
+        print(f"Password length cannot shorter than {minimum_length} characters")
+        password = input(f"Enter your password(no shorter than {minimum_length} characters): ")
+    return password
+
+
+main()
