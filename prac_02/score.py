adddbe26666f7ab94902abb5277197156b9753f5
Wang Runze <1678626701a@gmail.com>
2025-05-27 13:42:20 +0800

    Refactor score into functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,16 +2,25 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+from random import uniform
+SCORE_LOW = 0
+SCORE_HIGH = 100
+def main():
+    user_score = float(input("Enter score: "))
+    while user_score < SCORE_LOW or user_score > SCORE_HIGH:
+        print("Invalid score")
+        user_score = float(input("Enter score: "))
+    print(f"Your score: {user_score}\nYour grade: {determine_grade(user_score)}")
+    random_score = uniform(SCORE_LOW, SCORE_HIGH)
+    print(f"Random score: {random_score:.1f}\nRandom grade: {determine_grade(random_score)}")
 
+def determine_grade(score):
+    """Determine user's grade according to the score"""
+    if score < 50:
+        return "Bad"
+    if score <= 90:
+        return "Passable"
+    else:
+        return "Excellent"
 
-score = float(input("Enter score: "))
-while score < 0 or score > 100:
-    print("Invalid score")
-    score = float(input("Enter score: "))
-
-if score < 50:
-    print("Bad")
-elif score <= 90:
-    print("Passable")
-else:
-    print("Excellent")
+main()
