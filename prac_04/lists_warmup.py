8382d61de8b36800c6d36a66ce5b5dc74ad9cc50
Wang Runze <1678626701a@gmail.com>
2025-06-10 11:11:03 +0800

    Complete Warm-Up questions

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,12 @@
+numbers = ["ten", 1, 4, 1, 5, 9, 1]
+# assert numbers[0] == 3
+# assert numbers[-1] == 2
+# assert numbers[3] == 1
+# assert numbers[:-1] == [3, 1, 4, 1, 5, 9]
+# assert numbers[3:4] == [1]
+# assert (5 in numbers) == True
+# assert (7 in numbers) == False
+# assert ("3" in numbers) == False
+# assert numbers + [6, 5, 3] == [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+print(numbers[2:])
+print(9 in numbers)
