58c0ba29f50e3089ea7af9bb7805959ffbdf3550
Wang Runze <1678626701a@gmail.com>
2025-06-12 14:03:54 +0800

    Add json prac which solves my problem

---
+++ prac_05/currency_SGD_to_CNY.py
@@ -0,0 +1,14 @@
+import requests
+
+THRESHOLD = 5.5
+
+URL = 'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/sgd.json'
+response = requests.get(URL)
+country_to_exchange_rate = response.json()
+# print(country_to_exchange_rate)
+current_exchange_rate = country_to_exchange_rate['sgd']['cny']
+print(current_exchange_rate)
+if current_exchange_rate < THRESHOLD:
+    print("Time to buy some SGD :)")
+else:
+    print("To expensive :(")
