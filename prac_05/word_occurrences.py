4b0c86d4597e4b4d56b32debdaf2ebbe13d0dab8
Wang Runze <1678626701a@gmail.com>
2025-06-18 13:52:27 +0800

    Complete word occurrences exercise

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,5 +1,14 @@
 """
 Word Occurrences
 Estimate: 20 minutes
-Actual:    minutes
+Actual:   20 minutes
 """
+from operator import itemgetter
+input_text = input("Text: ")
+word_to_count = {}
+words = input_text.split(" ")
+max_word_length = max(len(word) for word in words)
+for word in words:
+    word_to_count[word] = word_to_count.get(word, 0) + 1
+for key, value in sorted(word_to_count.items(), key=itemgetter(0)):
+    print(f"{key:{max_word_length}} = {value}")
