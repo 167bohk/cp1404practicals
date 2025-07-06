6351649d328ec1701ddd56c37a2565cd4969b083
Wang Runze <1678626701a@gmail.com>
2025-07-06 09:44:47 +0800

    Add __init__, __str__ methods to class Guitar

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -3,3 +3,13 @@ Guitar
 Estimate: 30 minutes
 Actual:    minutes
 """
+class Guitar:
+    def __init__(self, name, year, cost):
+        """Initialize a Guitar instance."""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Convert a Guitar object into a string."""
+        return f"{self.name} ({self.year}) : ${self.cost:,}"
