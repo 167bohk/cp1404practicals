dba27e02feff72b3bec75dac482bb990a09aa0de
Wang Runze <1678626701a@gmail.com>
2025-07-07 18:08:46 +0800

    Complete more guitar exercise

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -1,9 +1,11 @@
 """
 more guitars
 Estimate: 30 minutes
-Actual:    minutes
+Actual:   30 minutes
 """
 import csv
+from datetime import datetime
+
 from prac_07.guitar import Guitar
 
 FILENAME = "guitars.csv"
@@ -13,6 +15,34 @@ def main():
     guitars.sort()
     for guitar in guitars:
         print(guitar)
+    add_new_guitars(guitars)
+    guitars.sort()
+    write_to_file(FILENAME, guitars)
+
+
+def add_new_guitars(guitars):
+    """Get new guitars and add them to the list guitars."""
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitars.append(Guitar(name, year, cost))
+        print(f"{name} ({year}) : ${cost:,.2f} added.")
+        print()
+        name = input("Name: ")
+    # guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    # guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    name_width = max(len(guitar.name) for guitar in guitars)
+    cost_width = max(len(f"{guitar.cost:,.2f}") for guitar in guitars)
+    print()
+    print("... snip ...")
+    print()
+    print("These are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = " (vintage)" if guitar.is_vintage(datetime.today().year) else ""
+        print(
+            f"Guitar {i}:  {guitar.name:>{name_width}} ({guitar.year}), worth $ {guitar.cost:{cost_width},.2f}{vintage_string}")
 
 
 def read_file(filename):
@@ -25,6 +55,15 @@ def read_file(filename):
             guitars.append(Guitar(row[0], int(row[1]), float(row[2])))
     return guitars
 
-
+def write_to_file(filename, guitars):
+    """Write Guitars to a file."""
+    with open(filename, "w", newline="") as out_file:
+        # for guitar in guitars:
+        #     print(f"{guitar.name},{guitar.year},{guitar.cost}", file=out_file)
+        rows = []
+        for guitar in guitars:
+            rows.append([guitar.name, guitar.year, guitar.cost])
+        writer = csv.writer(out_file)
+        writer.writerows(rows)
 
 main()
