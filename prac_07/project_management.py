cd6ea906a52df395aca7702a82efc92808a1dda4
Wang Runze <1678626701a@gmail.com>
2025-07-08 13:12:11 +0800

    Add get_valid_integer function

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -4,6 +4,7 @@ Estimate: 2 hours
 Actual:
 """
 import datetime
+from operator import attrgetter
 from prac_07.project import Project
 
 FILENAME = "projects.txt"
@@ -24,37 +25,20 @@ def main():
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "L":
-            loading_choice = input(f"Would you like to load from {FILENAME}? ").upper()
-            if loading_choice == "YES":
-                read_file(FILENAME)
-            else:
-                print("Where do you want to load from?")
-                is_valid_filename = False
-                loading_filename = input("Enter the filename: ")
-                while not is_valid_filename:
-                    try:
-                        read_file(loading_filename)
-                        is_valid_filename = True
-                    except FileNotFoundError:
-                        print(f"No such file or directory called {loading_filename}")
-                        loading_filename = input("Enter the filename: ")
+            load_projects()
         elif choice == "S":
-            saving_choice = input(f"Would you like to save to {FILENAME}? ").upper()
-            if saving_choice == "YES":
-                write_file(FILENAME, projects)
-            else:
-                print("Where do you want to save?")
-                out_filename = input("Enter the filename: ")
-                write_file(out_filename, projects)
-
+            save_projects(projects)
         elif choice == "D":
             display_projects(projects)
         elif choice == "F":
-            pass
+            filter_projects(projects)
         elif choice == "A":
             pass
         elif choice == "U":
-            pass
+            for i,project in enumerate(projects):
+                print(f"{i} {project}")
+            project_choice = get_valid_integer("Project choice: ", 0, len(projects)-1 )
+
         else:
             print("Invalid choice!")
         print(MENU)
@@ -63,6 +47,44 @@ def main():
     if saving_choice == "YES":
         write_file(FILENAME, projects)
     print("Thank you for using custom-built project management software.")
+
+
+def filter_projects(projects):
+    """Filter projects by date."""
+    threshold_date = get_valid_date_object("Show projects that start after date (dd/mm/yyyy): ")
+    recent_projects = sorted([project for project in projects if project.is_recent(threshold_date)],
+                             key=attrgetter("start_date"))
+    for recent_project in recent_projects:
+        print(recent_project)
+
+
+def save_projects(projects):
+    """Save projects to default file or a user selected file."""
+    saving_choice = input(f"Would you like to save to {FILENAME}? ").upper()
+    if saving_choice == "YES":
+        write_file(FILENAME, projects)
+    else:
+        print("Where do you want to save?")
+        out_filename = input("Enter the filename: ")
+        write_file(out_filename, projects)
+
+
+def load_projects():
+    """Load projects from default file or a user selected file."""
+    loading_choice = input(f"Would you like to load from {FILENAME}? ").upper()
+    if loading_choice == "YES":
+        read_file(FILENAME)
+    else:
+        print("Where do you want to load from?")
+        is_valid_filename = False
+        loading_filename = input("Enter the filename: ")
+        while not is_valid_filename:
+            try:
+                read_file(loading_filename)
+                is_valid_filename = True
+            except FileNotFoundError:
+                print(f"No such file or directory called {loading_filename}")
+                loading_filename = input("Enter the filename: ")
 
 
 def display_projects(projects):
@@ -110,5 +132,30 @@ def write_file(filename, projects):
             print(line, file=out_file)
     print(f"Saved {len(projects)} projects to {filename}")
 
+def get_valid_date_object(prompt):
+    """Get a valid date object."""
+    is_valid_date_string = False
+    while not is_valid_date_string:
+        date_string = input(prompt)
+        try:
+            date = datetime.datetime.strptime(date_string, "%d/%m/%Y")
+            is_valid_date_string = True
+        except ValueError:
+            print("Invalid date!")
+    return date
+
+def get_valid_integer(input_prompt, lower_limit, upper_limit):
+    """Get a valid integer."""
+    is_valid_number = False
+    while not is_valid_number:
+        try:
+            integer = int(input(input_prompt).strip())
+            if integer >= lower_limit and integer <= upper_limit:
+                is_valid_number = True
+            else:
+                print(f"Number out of range.")
+        except ValueError:
+            print("Invalid input; enter a valid number")
+    return integer
 
 main()
