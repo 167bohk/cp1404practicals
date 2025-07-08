328732d9a34dc711bc22c33e403b1a894dc31bf6
Wang Runze <1678626701a@gmail.com>
2025-07-08 10:01:06 +0800

    Complete class Project with useful methods

---
+++ prac_07/project.py
@@ -0,0 +1,27 @@
+import datetime
+
+
+class Project:
+    """Represent information about a project."""
+
+    def __init__(self, name="", strat_date=datetime.datetime.today().date(), priority=0, cost_estimate=0.0,
+                 completion_percentage=0):
+        """Initialize a Project object."""
+        self.name = name
+        self.strat_date = strat_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
+
+    def __str__(self):
+        """Return a Project object in string format."""
+        return f"{self.name}, start: {self.strat_date.strftime("%d/%m/%Y")}, priority {self.priority}, estimate: ${self.cost_estimate:,.2f}, completion: {self.completion_percentage}%"
+
+    def __lt__(self, other):
+        """Determine whether a Project has less priority than another one."""
+        return self.priority < other.priority
+
+    def is_recent(self, threshold=datetime.datetime.today().date()):
+        """Determine whether a project is recent, depending on the threshold given by user."""
+        return self.strat_date > threshold
+
