d1c8e90974db86108636b20cd176ebc4ba60d6c5
Wang Runze <1678626701a@gmail.com>
2025-07-22 12:15:30 +0800

    Round fare in taxi get_fare; Add missing docstrings

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -1,5 +1,5 @@
 from prac_09.silver_service_taxi import SilverServiceTaxi
 sst = SilverServiceTaxi(2, name="Hummer", fuel=200)
 sst.drive(18)
-assert sst.get_fare() == 48.78
+assert sst.get_fare() == 48.80
 print(sst)
