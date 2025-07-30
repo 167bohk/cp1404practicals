df38b5e3d986d5fbb6b333a8d13d0b8edb0f6096
Wang Runze <1678626701a@gmail.com>
2025-07-30 18:32:26 +0800

    Delete unnecessary imports

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,26 +1,19 @@
 import wikipedia
-from wikipedia import PageError, DisambiguationError
 
+searching_input = input("Enter page title: ")
+while searching_input != '':
+    try:
+        page_title = wikipedia.page(searching_input, auto_suggest=False)
+        print(wikipedia.summary(page_title))
+        print(wikipedia.page(page_title).url)
 
-def main():
-    """"""
+    except wikipedia.exceptions.DisambiguationError as e:
+        print("We need a more specific title. Try one of the following, or a new search:")
+        print(e.options)
+    except wikipedia.exceptions.PageError:
+        print(f'Page id "{searching_input}" does not match any pages. Try another id!')
+
     searching_input = input("Enter page title: ")
-    while searching_input != '':
-        try:
-            page_title = wikipedia.page(searching_input, auto_suggest=False)
-            print(wikipedia.summary(page_title))
-            print(wikipedia.page(page_title).url)
-
-        except wikipedia.exceptions.DisambiguationError as e:
-            print("We need a more specific title. Try one of the following, or a new search:")
-            print(e.options)
-        except wikipedia.exceptions.PageError:
-            print(f'Page id "{searching_input}" does not match any pages. Try another id!')
+print('Thank you.')
 
 
-
-        searching_input = input("Enter page title: ")
-    print('Thank you.')
-
-
-main()
